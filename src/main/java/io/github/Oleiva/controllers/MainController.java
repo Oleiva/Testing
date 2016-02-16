9e3addce0f2400f050eb6a7d92007eea8fb71f85
Oleg Ivashko <Oleg.Ivashko@yandex.ua>
2016-02-16 19:39:20 +0200

    add Loger

--- src/main/java/io/github/Oleiva/controllers/MainController.java
+++ src/main/java/io/github/Oleiva/controllers/MainController.java
@@ -1,17 +1,19 @@
 package io.github.Oleiva.controllers;
 
 import io.github.Oleiva.services.ItemsService;
-import io.github.Oleiva.utils.DBInitializer;
+//import io.github.Oleiva.utils.DBInitializer;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+
 @Controller
 public class MainController {
 
-//  @Autowired
-  private   DBInitializer dbInitializer;
+   public final Logger LOG = Logger.getLogger(this.getClass());
 
   @Autowired
   private ItemsService itemsService;
@@ -23,23 +25,23 @@ public class MainController {
         "<a href='http://oleiva.github.io'>Ivasoft </a> :)";
   }
 
-
   @RequestMapping("/initializer")
   @ResponseBody
+
   public void DBInitializer() {
-
     try{
-      itemsService.addItems("dd", "NAME",232323,323);
-      itemsService.addItems("dd", "NAME",232323,323);
-      itemsService.addItems("dd", "NAME",232323,323);
-      itemsService.addItems("dd", "NAME",232323,323);
-      itemsService.addItems("dd", "NAME",232323,323);
-      itemsService.addItems("dd", "NAME",232323,323);
-      itemsService.addItems("dd", "NAME",232323,323);
-      itemsService.addItems("dd", "NAME",232323,323);
-      itemsService.addItems("dd", "NAME",232323,323);
-
-
+      //(String SKU, String NAME, long PRICE, long AMOUN);
+      itemsService.addItems("10001-1290-S", "Oleg",  50000,30);
+      itemsService.addItems("10002-1233-D", "Andrey",61000,30);
+      itemsService.addItems("10031-1233-S", "Max",61000,30);
+      itemsService.addItems("10003-1234-D", "Bill",61000,30);
+      itemsService.addItems("10303-1235-S", "Mask",8000,30);
+      itemsService.addItems("10301-1236-M", "Alf",600,30);
+      itemsService.addItems("10031-1237-S", "Evgen",63000,30);
+      itemsService.addItems("13005-1238-S", "Allon",61030,30);
+      itemsService.addItems("10006-1239-S", "Dima",61032,30);
+      itemsService.addItems("10404-1210-M", "Sveta",61050,30);
+      itemsService.addItems("10051-1220-M", "Sveta",61500,30);
     }catch (Exception ex) {
     System.out.println("#dederedfedededed");
     }
@@ -47,7 +49,8 @@ public class MainController {
 
 
 
-  }
 
+
+  }
 
 }
--- src/main/java/io/github/Oleiva/utils/DBInitializer.java
+++ src/main/java/io/github/Oleiva/utils/DBInitializer.java
@@ -1,88 +1,88 @@
-package io.github.Oleiva.utils;
-
-/**
- * Created by OleIva on 16.02.2016.
- */
+//package io.github.Oleiva.utils;
+//
+///**
+// * Created by OleIva on 16.02.2016.
+// */
+////public class DBInitializer {
+////}
+//
+////package io.github.oleiva.utils;
+//
+//import io.github.Oleiva.dao.ItemsDao;
+//import io.github.Oleiva.entity.ItemsEntity;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import java.util.Date;
+//
+////import io.github.oleiva.dao.UserDao;
+////import io.github.oleiva.dao.NewsEntryDao;
+////import io.github.oleiva.entity.NewsEntry;
+////import io.github.oleiva.entity.User;
+//
+////import org.springframework.security.crypto.password.PasswordEncoder;
+//
+///**
+// * Initialize the database with some test entries.
+// */
+//
 //public class DBInitializer {
-//}
-
-//package io.github.oleiva.utils;
-
-import io.github.Oleiva.dao.ItemsDao;
-import io.github.Oleiva.entity.ItemsEntity;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Date;
-
-//import io.github.oleiva.dao.UserDao;
-//import io.github.oleiva.dao.NewsEntryDao;
-//import io.github.oleiva.entity.NewsEntry;
-//import io.github.oleiva.entity.User;
-
-//import org.springframework.security.crypto.password.PasswordEncoder;
-
-/**
- * Initialize the database with some test entries.
- */
-
-public class DBInitializer {
-
-//	private NewsEntryDao newsEntryDao;
-//	private UserDao userDao;
-	@Autowired
-	private ItemsDao itemsDao;
-//	private PasswordEncoder passwordEncoder;
-
-
-	protected DBInitializer() {
-		/* Default constructor for reflection instantiation */
-	}
-
-
-
-//	public DBInitializer(UserDao userDao, NewsEntryDao newsEntryDao, PasswordEncoder passwordEncoder) {
-//		this.userDao = userDao;
-//		this.newsEntryDao = newsEntryDao;
-//		this.passwordEncoder = passwordEncoder;
+//
+////	private NewsEntryDao newsEntryDao;
+////	private UserDao userDao;
+//	@Autowired
+//	private ItemsDao itemsDao;
+////	private PasswordEncoder passwordEncoder;
+//
+//
+//	protected DBInitializer() {
+//		/* Default constructor for reflection instantiation */
 //	}
-
-
-
-
-	public void initDataBase() {
-//		User userUser = new User("user", this.passwordEncoder.encode("user"));
-//		userUser.addRole("user");
-//		this.userDao.save(userUser);
 //
-//		User adminUser = new User("admin", this.passwordEncoder.encode("admin"));
-//		adminUser.addRole("user");
-//		adminUser.addRole("admin");
-//		this.userDao.save(adminUser);
 //
-//		long timestamp = System.currentTimeMillis() - 1000 * 60 * 60 * 24;
-//		for (int i = 0; i < 10; i++) {
-//			NewsEntry newsEntry = new NewsEntry();
-//			newsEntry.setContent("This is example content " + i);
-//			newsEntry.setDate(new Date(timestamp));
-//			this.newsEntryDao.save(newsEntry);
-//			timestamp += 1000 * 60 * 60;
+//
+////	public DBInitializer(UserDao userDao, NewsEntryDao newsEntryDao, PasswordEncoder passwordEncoder) {
+////		this.userDao = userDao;
+////		this.newsEntryDao = newsEntryDao;
+////		this.passwordEncoder = passwordEncoder;
+////	}
+//
+//
+//
+//
+//	public void initDataBase() {
+////		User userUser = new User("user", this.passwordEncoder.encode("user"));
+////		userUser.addRole("user");
+////		this.userDao.save(userUser);
+////
+////		User adminUser = new User("admin", this.passwordEncoder.encode("admin"));
+////		adminUser.addRole("user");
+////		adminUser.addRole("admin");
+////		this.userDao.save(adminUser);
+////
+////		long timestamp = System.currentTimeMillis() - 1000 * 60 * 60 * 24;
+////		for (int i = 0; i < 10; i++) {
+////			NewsEntry newsEntry = new NewsEntry();
+////			newsEntry.setContent("This is example content " + i);
+////			newsEntry.setDate(new Date(timestamp));
+////			this.newsEntryDao.save(newsEntry);
+////			timestamp += 1000 * 60 * 60;
+////		}
+//
+//	}
+//		public void initDataItems () {
+//
+//			String SKU = "dd";
+//			String NAME = "dd";
+//			long PRICE = 10000;
+//			long AMOUNT = 3434;
+//
+//			ItemsEntity itemsEntity = new ItemsEntity(SKU,NAME,PRICE,AMOUNT);
+//			System.out.println(itemsEntity);
+//
+//			this.itemsDao.saveAndFlush(itemsEntity);
 //		}
-
-	}
-		public void initDataItems () {
-
-			String SKU = "dd";
-			String NAME = "dd";
-			long PRICE = 10000;
-			long AMOUNT = 3434;
-
-			ItemsEntity itemsEntity = new ItemsEntity(SKU,NAME,PRICE,AMOUNT);
-			System.out.println(itemsEntity);
-
-			this.itemsDao.saveAndFlush(itemsEntity);
-		}
-
-
-		}
-
-
+//
+//
+//		}
+//
+//
